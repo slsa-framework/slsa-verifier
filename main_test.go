356ced849d753111b1ad38cf7f8edf927e904f08
Asra Ali <asraa@google.com>
2022-06-21 07:42:55 -0500

    update v1.1.1 testcases
    Signed-off-by: Asra Ali <asraa@google.com>

--- main_test.go
+++ main_test.go
@@ -22,7 +22,7 @@ func pString(s string) *string {
 
 // Versions of the builders to test.
 // TODO: Test v0.0.2.
-var generatorVersions = []string{"v1.0.0"}
+var generatorVersions = []string{"v1.1.1"}
 
 func Test_runVerify(t *testing.T) {
 	t.Parallel()
@@ -330,7 +330,7 @@ func Test_runVerify(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt // Re-initializing variable so it is not changed while executing the closure below
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
+			// t.Parallel()
 
 			checkVersions := generatorVersions
 			if tt.noversion {
