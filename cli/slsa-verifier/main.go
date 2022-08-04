b55c38291b4e6ca6c267b8352744c3816b389367
laurentsimon <laurentsimon@google.com>
2022-08-04 01:38:56 +0000

    update

--- cli/slsa-verifier/main.go
+++ cli/slsa-verifier/main.go
@@ -96,7 +96,7 @@ func runVerify(artifactPath, provenancePath, source, branch string,
 
 	provenance, err := os.ReadFile(provenancePath)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 
 	h := sha256.New()
