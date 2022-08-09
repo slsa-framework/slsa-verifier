0e29757da15ad795ba8cc759a16cb80501428b41
laurentsimon <laurentsimon@google.com>
2022-08-09 21:09:19 +0000

    update

--- cli/slsa-verifier/main_test.go
+++ cli/slsa-verifier/main_test.go
@@ -39,7 +39,7 @@ func Test_runVerify(t *testing.T) {
 		name        string
 		artifact    string
 		source      string
-		branch      string
+		pbranch     *string
 		ptag        *string
 		pversiontag *string
 		pbuilderID  *string
@@ -76,14 +76,19 @@ func Test_runVerify(t *testing.T) {
 			name:     "valid main branch set",
 			artifact: "binary-linux-amd64-workflow_dispatch",
 			source:   "github.com/slsa-framework/example-package",
-			branch:   "main",
+			pbranch:  pString("main"),
 		},
 		{
 			name:     "wrong branch master",
 			artifact: "binary-linux-amd64-workflow_dispatch",
 			source:   "github.com/slsa-framework/example-package",
-			branch:   "master",
+			pbranch:  pString("master"),
 			err:      serrors.ErrorMismatchBranch,
+		},
+		{
+			name:     "branch master not verified",
+			artifact: "binary-linux-amd64-workflow_dispatch",
+			source:   "github.com/slsa-framework/example-package",
 		},
 		{
 			name:     "wrong source append A",
@@ -352,7 +357,7 @@ func Test_runVerify(t *testing.T) {
 			name:      "e2e test repository verified with builder at head",
 			artifact:  "binary-linux-amd64-e2e-builder-repo",
 			source:    "github.com/slsa-framework/example-package",
-			branch:    "main",
+			pbranch:   pString("main"),
 			noversion: true,
 			builderID: "https://github.com/slsa-framework/slsa-github-generator/.github/workflows/builder_go_slsa3.yml",
 		},
@@ -378,12 +383,29 @@ func Test_runVerify(t *testing.T) {
 			err:       serrors.ErrorNoValidRekorEntries,
 			noversion: true,
 		},
-		// Regression test of sharded UUID
+		// annotated tags.
+		{
+			name:        "annotated tag",
+			artifact:    "annotated-tag-binary",
+			source:      "github.com/sethmlarson/python-slsa-release-test",
+			pversiontag: pString("0.1.9"),
+			noversion:   true,
+		},
+		{
+			name:        "no branch",
+			artifact:    "annotated-tag-binary",
+			source:      "github.com/sethmlarson/python-slsa-release-test",
+			pversiontag: pString("0.1.9"),
+			pbranch:     pString("main"),
+			err:         serrors.ErrorMismatchBranch,
+			noversion:   true,
+		},
+		// Regression test of sharded UUID.
 		{
 			name:      "regression: sharded uuids",
 			artifact:  "binary-linux-amd64-sharded",
 			source:    "github.com/slsa-framework/slsa-verifier",
-			branch:    "release/v1.0",
+			pbranch:   pString("release/v1.0"),
 			noversion: true,
 		},
 	}
@@ -429,18 +451,13 @@ func Test_runVerify(t *testing.T) {
 			}
 
 			for _, v := range checkVersions {
-				var pbranch *string
-				branch := tt.branch
-				if tt.branch != "" {
-					pbranch = &branch
-				}
 
 				artifactPath := filepath.Clean(filepath.Join(TEST_DIR, v, tt.artifact))
 				provenancePath := fmt.Sprintf("%s.intoto.jsonl", artifactPath)
 
 				_, builderID, err := runVerify(artifactPath,
 					provenancePath,
-					tt.source, pbranch, tt.pbuilderID,
+					tt.source, tt.pbranch, tt.pbuilderID,
 					tt.ptag, tt.pversiontag)
 
 				if !errCmp(err, tt.err) {
