d6f11e79270a1e554df80f8de8f2ef06d356d449
laurentsimon <laurentsimon@google.com>
2022-08-23 21:14:19 +0000

    update

--- cli/slsa-verifier/main_test.go
+++ cli/slsa-verifier/main_test.go
@@ -476,6 +476,7 @@ func Test_runVerifyArtifactPath(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt // Re-initializing variable so it is not changed while executing the closure below
 		t.Run(tt.name, func(t *testing.T) {
+			// Avoid rate limiting by not running the tests in parallel.
 			// t.Parallel()
 
 			checkVersions := getBuildersAndVersions(t, tt.minversion, tt.builders, ARTIFACT_PATH_BUILDERS)
