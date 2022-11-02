860fd674caacfc110b75be1f0065cc6ccace2020
Asra Ali <asraa@google.com>
2022-11-02 12:51:30 -0500

    fix
    Signed-off-by: Asra Ali <asraa@google.com>

--- cli/slsa-verifier/main_test.go
+++ cli/slsa-verifier/main_test.go
@@ -415,7 +415,7 @@ func Test_runVerifyGHAArtifactPath(t *testing.T) {
 			name:      "malicious: invalid signature expired certificate",
 			artifact:  "binary-linux-amd64-expired-cert",
 			source:    "github.com/slsa-framework/example-package",
-			err:       serrors.ErrorNoValidRekorEntries,
+			err:       serrors.ErrorRekorSearch,
 			noversion: true,
 		},
 		// Annotated tags.
