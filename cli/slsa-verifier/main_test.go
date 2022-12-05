32bdcec0a2753995f1ca75728a2bb35541e7c528
laurentsimon <laurentsimon@google.com>
2022-12-05 17:57:39 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- cli/slsa-verifier/main_test.go
+++ cli/slsa-verifier/main_test.go
@@ -1037,7 +1037,7 @@ func Test_runVerifyGCBArtifactImage(t *testing.T) {
 			source:     "github.com/laurentsimon/gcb-tests",
 			err:        serrors.ErrorNoValidSignature,
 		},
-		// TODO(221): verify the correct provenance is returned.
+		// TODO(388): verify the correct provenance is returned.
 		// This should also be done for all other entries in this test.
 		{
 			name:       "multiple provenance 2nd valid",
