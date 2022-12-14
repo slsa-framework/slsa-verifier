f0aec773a6cc860bc74c718886dab223046b7c61
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-12-14 14:34:54 -0800

    update (#410)
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- .github/workflows/scripts/schedule.actions/verifier-installer.sh
+++ .github/workflows/scripts/schedule.actions/verifier-installer.sh
@@ -8,7 +8,7 @@ minimum_version="$MINIMUM_INSTALLER_VERSION"
 list=""
 # Check the releases.
 echo "Listing releases"
-release_list=$(gh release list)
+release_list=$(gh -R "$GITHUB_REPOSITORY" release list)
 while read -r line; do
     tag=$(echo "$line" | cut -f1)
     if version_ge "$tag" "$minimum_version"; then
