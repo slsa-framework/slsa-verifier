0b14fd7309a77eee9348c670d665c6da066b61e7
Mihai Maruseac <mihaimaruseac@google.com>
2022-12-28 09:04:35 -0800

    Remove limit of number of artifacts to verify
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- cli/slsa-verifier/verify.go
+++ cli/slsa-verifier/verify.go
@@ -32,13 +32,7 @@ func verifyArtifactCmd() *cobra.Command {
 	o := &verify.VerifyOptions{}
 
 	cmd := &cobra.Command{
-		Use: "verify-artifact [flags] artifact",
-		Args: func(cmd *cobra.Command, args []string) error {
-			if len(args) != 1 {
-				return errors.New("expects a single path to an artifact")
-			}
-			return nil
-		},
+		Use: "verify-artifact [flags] artifact [artifact..]",
 		Short: "Verifies SLSA provenance on an artifact blob",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			v := verify.VerifyArtifactCommand{
