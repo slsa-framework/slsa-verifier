25a27a10330ccf6f55029c9b977971aa15ddfb1a
Kevin Halk <khalk@google.com>
2023-03-01 13:58:06 -0500

    Update SLSA verifier to support a global signing key for GCB V1 which creates the signature in a DSSE-conformant PAE format
    - new public key for "global PAE signing key"
    - test data and unit tests
    
    Signed-off-by: Kevin Halk <khalk@google.com>

--- verifiers/internal/gcb/provenance_test.go
+++ verifiers/internal/gcb/provenance_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/slsa-framework/slsa-verifier/v2/verifiers/utils"
 )
 
-// This function sets the statement of the proveannce, as if
+// This function sets the statement of the provenance, as if
 // it had been verified. This is necessary because individual functions
 // expect this statement to be populated; and this is done only
 // after the signature is verified.
@@ -552,6 +552,10 @@ func Test_VerifySignature(t *testing.T) {
 			path: "./testdata/gcloud-container-github.json",
 		},
 		{
+			name: "global gcb signing key",
+			path: "./testdata/gcloud-container-global-pae-signing-key-successful.json",
+		},
+		{
 			name:     "invalid signature",
 			path:     "./testdata/gcloud-container-invalid-signature.json",
 			expected: serrors.ErrorNoValidSignature,
@@ -559,6 +563,11 @@ func Test_VerifySignature(t *testing.T) {
 		{
 			name:     "invalid signature",
 			path:     "./testdata/gcloud-container-invalid-signature-payloadtype.json",
+			expected: serrors.ErrorNoValidSignature,
+		},
+		{
+			name:     "invalid signature - global PAE key",
+			path:     "./testdata/gcloud-container-invalid-signature-global-pae-key.json",
 			expected: serrors.ErrorNoValidSignature,
 		},
 		{
@@ -604,6 +613,10 @@ func Test_VerifySignature(t *testing.T) {
 			name: "signature multiple 3rd valid",
 			path: "./testdata/gcloud-container-multiple-signatures-3rdvalid.json",
 		},
+		{
+			name: "signature multiple global pae valid",
+			path: "./testdata/gcloud-container-multiple-signatures-global-pae-valid.json",
+		},
 	}
 	for _, tt := range tests {
 		tt := tt // Re-initializing variable so it is not changed while executing the closure below
@@ -623,7 +636,6 @@ func Test_VerifySignature(t *testing.T) {
 			if err := setStatement(prov); err != nil {
 				panic(fmt.Errorf("setStatement: %w", err))
 			}
-
 			err = prov.VerifySignature()
 			if !cmp.Equal(err, tt.expected, cmpopts.EquateErrors()) {
 				t.Errorf(cmp.Diff(err, tt.expected, cmpopts.EquateErrors()))
@@ -843,6 +855,10 @@ func Test_VerifyTextProvenance(t *testing.T) {
 		{
 			name: "valid gcb provenance",
 			path: "./testdata/gcloud-container-github.json",
+		},
+		{
+			name: "valid gcb provenance with global signing key",
+			path: "./testdata/gcloud-container-global-pae-signing-key-successful.json",
 		},
 		{
 			name:     "mismatch everything",
