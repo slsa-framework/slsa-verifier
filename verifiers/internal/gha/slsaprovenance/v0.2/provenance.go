d6c9936a0ae51f70f7e20ba1ee663cb6ec0d5622
Ian Lewis <ianlewis@google.com>
2023-06-26 05:24:46 +0000

    Remove old vars
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- verifiers/internal/gha/slsaprovenance/v0.2/provenance.go
+++ verifiers/internal/gha/slsaprovenance/v0.2/provenance.go
@@ -13,21 +13,6 @@ import (
 	"github.com/slsa-framework/slsa-verifier/v2/verifiers/internal/gha/slsaprovenance/iface"
 )
 
-var (
-	goBuilderBuildType = "https://github.com/slsa-framework/slsa-github-generator/go@v1"
-
-	genericGeneratorBuildType   = "https://github.com/slsa-framework/slsa-github-generator/generic@v1"
-	containerGeneratorBuildType = "https://github.com/slsa-framework/slsa-github-generator/container@v1"
-	npmCLIBuildType             = "https://github.com/npm/cli/gha@v1"
-
-	// Legacy build types.
-	legacyGoBuilderBuildType = "https://github.com/slsa-framework/slsa-github-generator-go@v1"
-	legacyBuilderBuildType   = "https://github.com/slsa-framework/slsa-github-generator@v1"
-
-	// byobBuildType is the base build type for BYOB delegated builders.
-	byobDelegatorBuildType = "https://github.com/slsa-framework/slsa-github-generator/delegator-generic@v0"
-)
-
 // Attestation is a SLSA v0.2 in-toto attestation statement.
 type Attestation struct {
 	intoto.StatementHeader
