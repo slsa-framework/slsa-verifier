9d0f2b2960115b012fc657a47b7076ece77d0810
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-05-13 23:13:52 +0000

    add specific test
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- verifiers/verifier_regression_test.go
+++ verifiers/verifier_regression_test.go
@@ -13,6 +13,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	sigstoreTuf "github.com/sigstore/sigstore-go/pkg/tuf"
 	serrors "github.com/slsa-framework/slsa-verifier/v2/errors"
 	"github.com/slsa-framework/slsa-verifier/v2/options"
 )
@@ -64,8 +65,6 @@ func Test_VerifyNpmPackage(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt // Re-initializing variable so it is not changed while executing the closure below
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
 			artifactPath := filepath.Clean(filepath.Join(TEST_DIR, "npm", "gha", tt.artifact))
 			attestationsPath := fmt.Sprintf("%s.json", artifactPath)
 			artifactHash, err := computeFileHash(artifactPath, sha256.New())
@@ -86,6 +85,35 @@ func Test_VerifyNpmPackage(t *testing.T) {
 				ExpectedID: &tt.builderID,
 			}
 			VerifyNpmPackage(context.Background(), attestations, artifactHash, provenanceOpts, builderOpts)
+		})
+
+		t.Run(tt.name+" - with sigstoreTufClient", func(t *testing.T) {
+			artifactPath := filepath.Clean(filepath.Join(TEST_DIR, "npm", "gha", tt.artifact))
+			attestationsPath := fmt.Sprintf("%s.json", artifactPath)
+			artifactHash, err := computeFileHash(artifactPath, sha256.New())
+			if err != nil {
+				t.Fatal(err)
+			}
+			attestationsReader, err := os.Open(attestationsPath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer attestationsReader.Close()
+			provenanceOpts := &options.ProvenanceOpts{
+				ExpectedSourceURI:      tt.source,
+				ExpectedDigest:         artifactHash,
+				ExpectedPackageName:    &tt.pkgName,
+				ExpectedPackageVersion: &tt.pkgVersion,
+			}
+			builderOpts := &options.BuilderOpts{
+				ExpectedID: &tt.builderID,
+			}
+			opts := sigstoreTuf.DefaultOptions().WithForceCache() // offline tests
+			sigastoreTufClient, err := sigstoreTuf.New(opts)
+			if err != nil {
+				t.Fatal(err)
+			}
+			VerifyNpmPackageWithSigstoreTufClient(context.Background(), attestationsReader, artifactHash, provenanceOpts, builderOpts, sigastoreTufClient)
 		})
 	}
 }
