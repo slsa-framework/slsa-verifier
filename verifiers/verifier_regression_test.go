f41e1fdd2ce1b22dc9c8f9ff9aaa969c4c82f1a9
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-05-17 21:56:00 +0000

    copy testdata
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- verifiers/verifier_regression_test.go
+++ verifiers/verifier_regression_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/slsa-framework/slsa-verifier/v2/options"
 )
 
-const TEST_DIR = "../cli/slsa-verifier/testdata"
+const testDir = "./testdata"
 
 // Test_VerifyNpmPackage ensures that verifiers.VerifyNpmPackage works,
 // borrowing only a few examples from the larger set of cases in main.Test_runVerifyGHAArtifactPath
@@ -66,7 +66,7 @@ func Test_VerifyNpmPackage(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt // Re-initializing variable so it is not changed while executing the closure below
 		t.Run(tt.name, func(t *testing.T) {
-			artifactPath := filepath.Clean(filepath.Join(TEST_DIR, "npm", "gha", tt.artifact))
+			artifactPath := filepath.Clean(filepath.Join(testDir, "npm", "gha", tt.artifact))
 			attestationsPath := fmt.Sprintf("%s.json", artifactPath)
 			artifactHash, err := computeFileHash(artifactPath, sha256.New())
 			if err != nil {
@@ -89,7 +89,7 @@ func Test_VerifyNpmPackage(t *testing.T) {
 		})
 
 		t.Run(tt.name+" - with sigstoreTufClient", func(t *testing.T) {
-			artifactPath := filepath.Clean(filepath.Join(TEST_DIR, "npm", "gha", tt.artifact))
+			artifactPath := filepath.Clean(filepath.Join(testDir, "npm", "gha", tt.artifact))
 			attestationsPath := fmt.Sprintf("%s.json", artifactPath)
 			artifactHash, err := computeFileHash(artifactPath, sha256.New())
 			if err != nil {
