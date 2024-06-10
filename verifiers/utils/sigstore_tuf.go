f1ee89d38309a45a39e1405d5054a0a924ccb84c
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-06-10 16:25:30 +0000

    remove NewSigstoreTufClient()
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- verifiers/utils/sigstore_tuf.go
+++ verifiers/utils/sigstore_tuf.go
@@ -1,21 +1,7 @@
 package utils
 
-import (
-	"fmt"
-
-	sigstoreTUF "github.com/sigstore/sigstore-go/pkg/tuf"
-)
-
+// SigstoreTUFClient is the interface for the Sigstore TUF client.
 type SigstoreTUFClient interface {
+	// GetTarget retrieves the target file from the TUF repository.
 	GetTarget(target string) ([]byte, error)
-}
-
-// NewSigstoreTUFClient gets a Sigstore TUF client, which itself is a wrapper around the official TUF client.
-func NewSigstoreTUFClient() (*sigstoreTUF.Client, error) {
-	opts := sigstoreTUF.DefaultOptions()
-	client, err := sigstoreTUF.New(opts)
-	if err != nil {
-		return nil, fmt.Errorf("creating SigstoreTUF client: %w", err)
-	}
-	return client, nil
 }
