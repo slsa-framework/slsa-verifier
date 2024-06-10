5845e1cfaa811c0118f9378a4b30996e795f66a6
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-06-10 17:47:10 +0000

    lint
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- options/options.go
+++ options/options.go
@@ -44,7 +44,7 @@ type BuilderOpts struct {
 
 // VerifierOpts are the options for the verifier, created with the VerifierOptioner functions.
 // In the future, this can include a logger and a rekor client,
-// or be a standalone argument to the verifier functions
+// or be a standalone argument to the verifier functions.
 type VerifierOpts struct {
 	// SigstoreTufClient is the Sigstore TUF client, used for retrieving the Npmjs public keys
 	SigstoreTUFClient apiUtils.SigstoreTUFClient
--- verifiers/internal/gha/npm_test.go
+++ verifiers/internal/gha/npm_test.go
@@ -18,12 +18,14 @@ import (
 	"github.com/slsa-framework/slsa-verifier/v2/verifiers/utils"
 )
 
-func init() {
-	// preload the default sigstore TUF client so that it is available and cached for the parallel tests
+// TestMain intercepts the test runner to run some setup code before running the tests.
+func TestMain(m *testing.M) {
+	// Initialize the default sigstore TUF client for parallel tests
 	_, err := getDefaultSigstoreTUFClient()
 	if err != nil {
 		panic(err)
 	}
+	os.Exit(m.Run())
 }
 
 func Test_verifyName(t *testing.T) {
