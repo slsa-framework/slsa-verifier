e18c94e4b96f510bc88f06a551ee73332be23804
Ville Skyttä <ville.skytta@iki.fi>
2025-06-20 09:09:30 +0300

    refactor: use maps.Copy
    Signed-off-by: Ville Skyttä <ville.skytta@iki.fi>

--- verifiers/utils/structures.go
+++ verifiers/utils/structures.go
@@ -1,12 +1,10 @@
 package utils
 
+import "maps"
+
 func MergeMaps[K comparable, V any](m1, m2 map[K]V) map[K]V {
 	m := make(map[K]V, len(m1)+len(m2))
-	for k, v := range m2 {
-		m[k] = v
-	}
-	for k, v := range m1 {
-		m[k] = v
-	}
+	maps.Copy(m, m2)
+	maps.Copy(m, m1)
 	return m
 }
